ecbee32e9ed24b811f4b49372da26643bddd183d
Roberto B <roberto.butti@gmail.com>
2021-09-21 20:31:23 +0200

    Ibis configuration

---
+++ ibis.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    /**
+     * The book title.
+     */
+    'title' => '50 drops of PHP',
+
+    /**
+     * The author name.
+     */
+    'author' => 'Roberto Butti',
+
+    /**
+     * The list of fonts to be used in the different themes.
+     */
+    'fonts' => [
+                //'calibri' => 'Calibri-Regular.ttf',
+                //'times' => 'times-regular.ttf',
+    ],
+
+    /**
+     * Document Dimensions.
+     */
+    'document' => [
+        'format' => [210, 297],
+        'margin_left' => 27,
+        'margin_right' => 27,
+        'margin_bottom' => 14,
+        'margin_top' => 14,
+    ],
+
+    /**
+     * Table of Contents Levels
+     */
+    'toc_levels' => [
+        'H1' => 0,
+        'H2' => 1,
+    ],
+
+    /**
+     * Cover photo position and dimensions
+     */
+    'cover' => [
+        'position' => 'position: absolute; left:0; right: 0; top: -.2; bottom: 0;',
+        'dimensions' => 'width: 210mm; height: 297mm; margin: 0;',
+        'image' => 'cover.png',
+    ],
+
+    /**
+     * Page ranges to be used with the sample command.
+     */
+    'sample' => [
+        [1, 3],
+        [80, 85],
+        [100, 103],
+    ],
+
+    /**
+     * A notice printed at the final page of a generated sample.
+     */
+    'sample_notice' => 'This is a sample from "Laravel Queues in Action" by Mohamed Said. <br>
+                        For more information, <a href="https://www.learn-laravel-queues.com/">Click here</a>.',
+];
