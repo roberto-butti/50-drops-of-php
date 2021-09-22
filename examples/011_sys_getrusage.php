ac35b89e06b819ee4c16d92db765917867965a14
Roberto B <roberto.butti@gmail.com>
2021-09-22 08:49:43 +0200

    PSR12 style

--- examples/011_sys_getrusage.php
+++ examples/011_sys_getrusage.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // $mode default, == 0
 $resources = getrusage();
 foreach ($resources as $key => $value) {
@@ -9,13 +8,13 @@ foreach ($resources as $key => $value) {
 
 
 // $mode 1
-$resources = getrusage( 1 );
+$resources = getrusage(1);
 foreach ($resources as $key => $value) {
     echo "=> " . $key . ": " . $value . PHP_EOL;
 }
 // $mode 1 after executing an external process
 exec('sleep 1');
-$resources = getrusage( 1 );
+$resources = getrusage(1);
 foreach ($resources as $key => $value) {
     echo "=> " . $key . ": " . $value . PHP_EOL;
 }
