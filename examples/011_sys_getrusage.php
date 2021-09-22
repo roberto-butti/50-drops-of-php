e18859fe9d8efac03b1b0545db6e2b43ae875a4e
Roberto B <roberto.butti@gmail.com>
2021-09-22 08:48:52 +0200

    Create 011_sys_getrusage.php

---
+++ examples/011_sys_getrusage.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// $mode default, == 0
+$resources = getrusage();
+foreach ($resources as $key => $value) {
+    echo "=> " . $key . ": " . $value . PHP_EOL;
+}
+
+
+// $mode 1
+$resources = getrusage( 1 );
+foreach ($resources as $key => $value) {
+    echo "=> " . $key . ": " . $value . PHP_EOL;
+}
+// $mode 1 after executing an external process
+exec('sleep 1');
+$resources = getrusage( 1 );
+foreach ($resources as $key => $value) {
+    echo "=> " . $key . ": " . $value . PHP_EOL;
+}
