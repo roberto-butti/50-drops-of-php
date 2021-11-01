61d90d560d6bbc4f6a37fef02e44a5b511f472d9
Roberto B <roberto.butti@gmail.com>
2021-11-01 22:21:08 +0100

    Update 204_var_get_object_vars.php

--- examples/204_var_get_object_vars.php
+++ examples/204_var_get_object_vars.php
@@ -1,3 +1,12 @@
 <?php
 
-// [TODO]
+$o =  new StdClass;
+$o->title = "Title";
+$o->subtitle = "Subtitle";
+
+foreach (get_object_vars($o) as $attributeName => $attributeValue) {
+    echo "Attribute:" . $attributeName .
+    ", value: " . $attributeValue .
+    ", type: " .gettype($attributeValue).PHP_EOL;
+}
+print_r(get_object_vars($o));
