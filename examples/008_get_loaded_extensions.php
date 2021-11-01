a70c0237814acc827d366b84eef8c6a4544ef843
Roberto B <roberto.butti@gmail.com>
2021-11-01 20:16:28 +0100

    fix get loaded extension

--- examples/008_get_loaded_extensions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$zend_extensions = true;
-$extensions = get_loaded_extensions($zend_extensions);
-foreach ($extensions as $key => $extension) {
-    echo "-> " . $extension . PHP_EOL;
-}
--- examples/008_sys_get_loaded_extensions.php
+++ examples/008_sys_get_loaded_extensions.php
@@ -1,3 +1,7 @@
 <?php
 
-// [TODO]
+$zend_extensions = true;
+$extensions = get_loaded_extensions($zend_extensions);
+foreach ($extensions as $key => $extension) {
+    echo "-> " . $extension . PHP_EOL;
+}
