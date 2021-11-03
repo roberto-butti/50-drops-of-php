1ec9165c5df47927a7c4dd7e537f51522a8c6261
Roberto B <roberto.butti@gmail.com>
2021-11-03 21:47:47 +0100

    arrow functions

--- content/302_fns_arrowfunction.md
+++ content/302_fns_arrowfunction.md
@@ -1,4 +1,30 @@
 ## Arrow Functions with PHP
 
+Arrow functions are similar to anonymous function, but they have a more concise syntax and limited usage.
+
+```php
+// Define a classical function
+function sum_classic($x, $y) {
+    return $x + $y;
+}
+// define an anonymous function
+$sum_anonymous = function ($x, $y) {
+    return $x + $y;
+};
+// define arrow function
+$sum_arrow = fn($x, $y) => $x+y;
+
+
+```
+
+Another difference between anonymous and the arrow function, is that the arrow function has visibility on the "external" scope. In the next example, 2 variables, $a and $b are declared and initialized outside the arrow function, and the are used in the expression of the arrow function.
+
+```php
+$a = 4;
+$b= 5;
+$sumx = fn() => $a + $b;
+echo $sumx() . PHP_EOL;
+```
+
 
 
--- examples/302_fns_arrowfunction.php
+++ examples/302_fns_arrowfunction.php
@@ -1,5 +1,29 @@
 <?php
 
+// Define a classical function
+function sum_classic($x, $y)
+{
+    return $x + $y;
+}
+// define an anonymous function
+$sum_anonymous = function ($x, $y) {
+    return $x + $y;
+};
+// define arrow function
+$sum_arrow = fn($x, $y) => $x + y;
+
+echo "Sum classic   : " . sum_classic(5, 2) . PHP_EOL;
+echo "Sum anonymous : " . $sum_anonymous(5, 2) . PHP_EOL;
+echo "Sum arrow     : " . $sum_anonymous(5, 2) . PHP_EOL;
+
+// example for showing the scope of variables
+$a = 4;
+$b = 5;
+$sumx = fn() => $a + $b;
+echo $sumx() . PHP_EOL;
+
+
+// a little bit more complex example
 $collection = [
     ['product' => 'Desk', 'price' => 200, 'active' => true],
     ['product' => 'Chair', 'price' => 100, 'active' => true],
@@ -7,7 +31,6 @@ $collection = [
     ['product' => 'Bookcase', 'price' => 150, 'active' => true],
     ['product' => 'Door', 'price' => 100, 'active' => true],
 ];
-
 
 $columns = ["product", "price"];
 $filteredArray = array_map(fn($item) => array_intersect_key($item, array_flip($columns)), $collection);
